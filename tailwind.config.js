53d429d91f18466b7c822a052efc91ca28485fc8
Kingostyl <emr8770@gmail.com>
2023-03-17 10:37:30 +0700

    Frist Commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {p: ['quicksand', 'sans-serif']},
+    boxShadow: {shadow: ['0 4px 8px 0 rgba(0, 0, 0, 0.2)']},
+    extend: {
+    },
+  },
+  plugins: [],
+}
